72ada98dbf8749bdf15ddd17b95c1e98391f5d3b
gaoyinxiao <gaoyinxiao@gohigh.com.cn>
2023-04-19 11:12:38 +0800

    还原

--- spug_api/spug/overrides.py
+++ spug_api/spug/overrides.py
@@ -1,36 +1,36 @@
 #  Database
-# DATABASES = {
-#     'default': {
-#         'ATOMIC_REQUESTS': True,
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'spug',  # 替换为自己的数据库名，请预先创建好编码为utf8mb4的数据库
-#         'USER': 'root',  # 数据库用户名
-#         'PASSWORD': '96vQkU88E_tfdS',  # 数据库密码
-#         'HOST': '10.188.15.192',  # 数据库地址
-#         'PORT': '3402',
-#         'OPTIONS': {
-#             'charset': 'utf8mb4',
-#             'sql_mode': 'STRICT_TRANS_TABLES',
-#             # 'unix_socket': '/opt/mysql/mysql.sock' # 如果是本机数据库,且不是默认安装的Mysql,需要指定Mysql的socket文件路径
-#         }
-#     }
-# }
-
-# 测试mysql数据库
 DATABASES = {
     'default': {
         'ATOMIC_REQUESTS': True,
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'spug',             # 替换为自己的数据库名，请预先创建好编码为utf8mb4的数据库
-        'USER': 'spug_user',        # 数据库用户名
-        'PASSWORD': '123456',  # 数据库密码
-        'HOST': '10.188.15.55',        # 数据库地址
+        'NAME': 'spug',  # 替换为自己的数据库名，请预先创建好编码为utf8mb4的数据库
+        'USER': 'root',  # 数据库用户名
+        'PASSWORD': '96vQkU88E_tfdS',  # 数据库密码
+        'HOST': '10.188.15.192',  # 数据库地址
+        'PORT': '3402',
         'OPTIONS': {
             'charset': 'utf8mb4',
             'sql_mode': 'STRICT_TRANS_TABLES',
+            # 'unix_socket': '/opt/mysql/mysql.sock' # 如果是本机数据库,且不是默认安装的Mysql,需要指定Mysql的socket文件路径
         }
     }
 }
+
+# 测试mysql数据库
+# DATABASES = {
+#     'default': {
+#         'ATOMIC_REQUESTS': True,
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'spug',             # 替换为自己的数据库名，请预先创建好编码为utf8mb4的数据库
+#         'USER': 'spug_user',        # 数据库用户名
+#         'PASSWORD': '123456',  # 数据库密码
+#         'HOST': '10.188.15.55',        # 数据库地址
+#         'OPTIONS': {
+#             'charset': 'utf8mb4',
+#             'sql_mode': 'STRICT_TRANS_TABLES',
+#         }
+#     }
+# }
 
 
 CACHES = {
