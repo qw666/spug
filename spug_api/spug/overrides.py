25ed8638866cbedf75b31435bc4b0bdf8b3d7169
wangcen <13701189186@163.com>
2023-04-25 09:19:57 +0800

    同步配置文件

--- spug_api/spug/overrides.py
+++ spug_api/spug/overrides.py
@@ -1,4 +1,5 @@
-#  Database
+DEBUG = False
+
 DATABASES = {
     'default': {
         'ATOMIC_REQUESTS': True,
@@ -16,30 +17,12 @@ DATABASES = {
     }
 }
 
-# 测试mysql数据库
-# DATABASES = {
-#     'default': {
-#         'ATOMIC_REQUESTS': True,
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'spug',             # 替换为自己的数据库名，请预先创建好编码为utf8mb4的数据库
-#         'USER': 'spug_user',        # 数据库用户名
-#         'PASSWORD': '123456',  # 数据库密码
-#         'HOST': '10.188.15.55',        # 数据库地址
-#         'OPTIONS': {
-#             'charset': 'utf8mb4',
-#             'sql_mode': 'STRICT_TRANS_TABLES',
-#         }
-#     }
-# }
-
-
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://10.188.15.56:6379/1",
+        "LOCATION": "redis://:85_wdV60pvRCAY@10.188.15.56:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-            "PASSWORD": "85_wdV60pvRCAY"
         }
     }
 }
